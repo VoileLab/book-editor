126656cac16494fa68042992b41a77708035352b
mudream4869 <mudream4869@gmail.com>
2024-03-23 14:32:26 +0800

    Tweet add content function

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -7,6 +7,8 @@
     <v-btn v-if="bookStore.contentAvailable" icon="mdi-trash-can" @click="deleteContent"> </v-btn>
     <v-btn v-if="bookStore.contentAvailable" icon="mdi-content-cut" @click="$refs.cutDialog.open()"></v-btn>
 
+    <v-btn v-if="bookStore.bookAvailable" icon="mdi-plus" @click="bookStore.addContent({index: bookStore.editState.currentBookIndex})"></v-btn>
+
     <v-spacer></v-spacer>
 
     <v-menu v-if="bookStore.bookAvailable">
