a47863e9d4ea8c91bd9331050cbc87d1a96df294
Mudream <mudream4869@gmail.com>
2024-03-27 23:14:29 +0800

    Use hash history mode, fix github node version

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,10 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHashHistory } from 'vue-router/auto'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
 })
 
 export default router
